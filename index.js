ff6b63c8474231eb48edb0479e5948881ee436c4
Mandeep-dk <mandeepdeka492@gmail.com>
2025-01-26 14:45:36 +0530

    Add password checking functionality in index.js

--- index.js
+++ index.js
@@ -11,4 +11,22 @@ let attempts = 3;
 
 console.log("Highly confidential code! You need to enter the correct password to unlock the confidential code.");
 
+function askPassword() {
+    rl.question("Enter the password: ", (input_pass)=>{
+        if(input_pass===process.env.PASSWORD){
+            console.log(`Access granted! The secret nuclear code is ${process.env.SECRET_CODE}`);
+            rl.close();
+        }else{
+            attempts--;
+            if(attempts>0){
+                console.log(`Incorrect password. You have ${attempts} attempt(s) remaining.`);
+                askPassword();
+            }else{
+                console.log('Out of attempts. Better luck next time.');
+                rl.close();
+            }
+        }
+    })
+}
 
+askPassword();
